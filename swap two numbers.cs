f3243c27b1bd2f9cfdf8e19924a0831dea5ca6f3
jenirgm9 <113678448+jenirgm9@users.noreply.github.com>
2022-11-28 17:50:32 +0530

    Create swap two numbers.cs

---
+++ swap two numbers.cs
@@ -0,0 +1,22 @@
+using system;
+class sample
+{
+public int value
+{
+get;
+set;
+}
+public sample(int passed value)
+{
+this.value
+}
+class program
+{
+static void main(String[]args)
+{
+sample a=new Sample(5);
+sample b=new Sample(10);
+b=a;
+console.writeLine(a.value);
+console.writeLine(b.value);
+}}
